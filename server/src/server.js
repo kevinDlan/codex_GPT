e65ead7b23318f0a4f53d74350e7d2dcb11897c3
kevinDlan <kevinkone19@gmail.com>
2023-01-08 17:40:44 +0000

    initial commit

---
+++ server/src/server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors'
+import{Configuration, OpenAIApi} from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration);
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Api home endpoint
+app.get('/', async (req, res)=>
+{
+    res.status(200).send({
+        message: 'Hello Codex'
+    })
+})
+
+app.post('/', async (req, res)=>
+{
+    try
+    {
+        const prompt = req.body.prompt;
+        
+        const response = await openai.createCompletion(
+                {
+                model:"text-davinci-003",
+                prompt:`${prompt}`,
+                temperature:0,
+                max_tokens:3000,
+                top_p:1,
+                frequency_penalty:0.5,
+                presence_penalty:0,
+                }
+            );
+        res.status(200).send({bot: response.data.choices[0].text});
+    }catch(error)
+    {
+        console.log(error);
+        res.status(500).send({error})
+    }
+});
+
+
+app.listen(process.env.APP_PORT, ()=>
+{
+    console.log(`server is running on http://localhost:${process.env.APP_PORT}`);
+})
